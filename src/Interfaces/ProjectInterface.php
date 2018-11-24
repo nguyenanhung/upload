cf2040d9a8ee7eefe1c43c0794f4ce08267ab6f9
713uk13m <713uk13m@713uk13ms-MacBook-Pro.local>
2018-11-24 19:02:48 +0700

    Update package

--- src/Interfaces/ProjectInterface.php
+++ src/Interfaces/ProjectInterface.php
@@ -17,8 +17,8 @@ namespace nguyenanhung\Upload\Interfaces;
  */
 interface ProjectInterface
 {
-    const VERSION                = '1.0';
-    const LAST_MODIFIED          = '2018-11-06';
+    const VERSION                = '1.0.1';
+    const LAST_MODIFIED          = '2018-11-24';
     const AUTHOR_NAME            = 'Hung Nguyen';
     const AUTHOR_EMAIL           = 'dev@nguyenanhung.com';
     const PROJECT_NAME           = 'Upload Manager';

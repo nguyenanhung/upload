030d99e29a3a63b287d4ab8abe81fb28fa3e336d
Hung Nguyen <dev@nguyenanhung.com>
2024-09-22 03:00:20 +0700

    Update Environment.php

--- src/Environment.php
+++ src/Environment.php
@@ -17,8 +17,8 @@ namespace nguyenanhung\Upload;
  */
 interface Environment
 {
-    const VERSION       = '2.0.1';
-    const LAST_MODIFIED = '2022-07-18';
+    const VERSION       = '2.0.2';
+    const LAST_MODIFIED = '2024-09-22';
     const AUTHOR_NAME   = 'Hung Nguyen';
     const AUTHOR_EMAIL  = 'dev@nguyenanhung.com';
     const AUTHOR_WEB    = 'https://nguyenanhung.com';
